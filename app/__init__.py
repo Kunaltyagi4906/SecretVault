73573e5dc4a7102c6e0d070cc912d45429e074a7
Kunal Tyagi <kunaltyagi4906@gmail.com>
2025-07-12 09:40:14 +0530

    Fix Docker CMD and README config

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,8 @@
 from flask import Flask
 from app.auth import auth
 from app.vault import vault
-
-# 👇 Import highlight filter
-from utils.filters import highlight  
 from app.routes import main
-app.register_blueprint(main)
+from utils.filters import highlight  
 
 def create_app():
     app = Flask(__name__, template_folder='../templates', static_folder='../static')
@@ -14,9 +11,9 @@ def create_app():
     # 🔐 Blueprints
     app.register_blueprint(auth)
     app.register_blueprint(vault)
+    app.register_blueprint(main)  # 👈 Yeh ab sahi jagah par hai
 
-    # ✨ Register custom Jinja filter
+    # ✨ Custom Jinja filter
     app.jinja_env.filters['highlight'] = highlight
 
     return app
-                                     
