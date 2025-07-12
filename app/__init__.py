6daddae89098cca396ace2f6ce309c2ae4d12d3b
Kunal Tyagi <kunaltyagi4906@gmail.com>
2025-07-12 13:09:25 +0530

    Fix blueprint order bug

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,17 @@
 from flask import Flask
 from app.auth import auth
 from app.vault import vault
-from app.routes import main
+from app.routes import main  # if you have it
 from utils.filters import highlight
 
 def create_app():
-    app = Flask(__name__, template_folder='../templates', static_folder='../static')
+    app = Flask(__name__, template_folder='templates', static_folder='static')
     app.secret_key = 'myvault-genz-rockzz-4321'
 
-    # 🔐 Blueprints
     app.register_blueprint(auth)
     app.register_blueprint(vault)
     app.register_blueprint(main)
 
-    # ✨ Custom Jinja filter
     app.jinja_env.filters['highlight'] = highlight
 
     return app
