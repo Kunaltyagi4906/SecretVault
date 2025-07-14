f4e272271015df2cd401fe382a872a0e6a7ea001
Kunal Tyagi <kunaltyagi4906@gmail.com>
2025-07-14 09:52:55 +0530

    chore: enable sqlite on huggingface

--- config/db.py
+++ config/db.py
@@ -1,9 +1,10 @@
 import os
 import sqlite3
 import mysql.connector
+print("🔁 USE_SQLITE =", os.environ.get("USE_SQLITE"))
 
 def get_db_connection():
-    use_sqlite = os.environ.get("USE_SQLITE", "False").lower() == "True"
+    use_sqlite = os.environ.get("USE_SQLITE", "false").lower() == "true"
 
     if use_sqlite:
         conn = sqlite3.connect("database.db")
