062d2d0d91f80d6bf0fef9a2d6ea211de3cc9373
Kunal Tyagi <kunaltyagi4906@gmail.com>
2025-07-14 09:46:09 +0530

    chore: enable sqlite on huggingface

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0", port=7860)
+print("🔁 USE_SQLITE =", os.environ.get("USE_SQLITE"))
