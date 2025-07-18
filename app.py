454a8138667b7214f1918281b5f7bab5426dd5ab
Kunal Tyagi <kunaltyagi4906@gmail.com>
2025-07-18 15:49:08 +0530

    chore: enable sqlite on huggingface

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-# app.py or main.py
-
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-from app import create_app  # make sure your 'app' folder has __init__.py
+from app import create_app
 
 app = create_app()
 
-# Hugging Face expects this:
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 7860)), debug=False)
-
-# Optional Debug
-print("✅ USE_SQLITE =", os.environ.get("USE_SQLITE"))
+    app.run(debug=False, host="0.0.0.0", port=7860)
+    
+    
+print("🔁 USE_SQLITE =", os.environ.get("USE_SQLITE"))
